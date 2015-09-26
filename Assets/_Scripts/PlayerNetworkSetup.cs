59a8e51940bc5d8503fab45cafc527be7ace45fd
LivingValkyrie <deadwynn@gmail.com>
2015-09-26 12:53:34 -0500

    first video complete
    Signed-off-by: LivingValkyrie <deadwynn@gmail.com>

---
+++ Assets/_Scripts/PlayerNetworkSetup.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+/// <summary>
+/// Author: Matt Gipson
+/// Contact: Deadwynn@gmail.com
+/// Domain: www.livingvalkyrie.com
+/// 
+/// Description: PlayerNetworkSetup 
+/// </summary>
+public class PlayerNetworkSetup : NetworkBehaviour {
+    #region Fields
+
+    [SerializeField]
+    Camera FPSCam;
+
+    [SerializeField]
+    AudioListener listener;
+
+    #endregion
+
+    void Start() {
+        if (isLocalPlayer) {
+            GameObject.Find("SceneCamera").SetActive(false);
+
+            //player components
+            GetComponent<CharacterController>().enabled = true;
+            GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = true;
+
+            //children components
+            FPSCam.enabled = true;
+            listener.enabled = true;
+
+        }
+    }
+    
+}
---
+++ Assets/_Scripts/PlayerSyncPosition.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+/// <summary>
+/// Author: Matt Gipson
+/// Contact: Deadwynn@gmail.com
+/// Domain: www.livingvalkyrie.com
+/// 
+/// Description: PlayerSyncPosition 
+/// </summary>
+public class PlayerSyncPosition : NetworkBehaviour {
+    #region Fields
+
+    [SyncVar]
+    Vector3 syncPos;
+
+    [SerializeField]
+    Transform myTransform;
+
+    [SerializeField]
+    float lerpRate = 15;
+
+    #endregion
+
+    void FixedUpdate() {
+        TransmitPosition();
+        LerpPosition();
+    }
+
+    void LerpPosition() {
+        if (!isLocalPlayer) {
+            myTransform.position = Vector3.Lerp(myTransform.position, syncPos, Time.deltaTime * lerpRate);
+        }
+    }
+
+    [Command]
+    void CmdProvidePositionToServer(Vector3 pos) {
+        syncPos = pos;
+    }
+
+    [ClientCallback]
+    void TransmitPosition() {
+        if (isLocalPlayer) {
+            CmdProvidePositionToServer(myTransform.position);
+        }
+    }
+}
