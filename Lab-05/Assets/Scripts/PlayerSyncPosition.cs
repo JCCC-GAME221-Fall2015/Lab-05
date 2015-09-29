e369ba9128b434ccba14b4775f95bfbbbd3a4cd0
JakeSkov <JakeSkov95@gmail.com>
2015-09-28 23:43:36 -0500

    JakeFinal

---
+++ Lab-05/Assets/Scripts/PlayerSyncPosition.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class PlayerSyncPosition : NetworkBehaviour 
+{
+    [SyncVar]
+    private Vector3 syncPos;
+    [SerializeField]
+    Transform myTransform;
+    [SerializeField]
+    float lerpRate = 15;
+
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+        TransmitPosition();
+        LerpPosition();
+	}
+
+    void LerpPosition()
+    {
+        if (!isLocalPlayer)
+        {
+            myTransform.position = Vector3.Lerp(myTransform.position, syncPos, Time.deltaTime * lerpRate);
+        }
+    }
+
+    [Command]
+    void CmdSyncPositionToServer(Vector3 pos)
+    {
+        syncPos = pos;
+    }
+
+    [ClientCallback]
+    void TransmitPosition()
+    {
+        if(isLocalPlayer)
+        {
+        CmdSyncPositionToServer(myTransform.position);
+        }
+    }
+}
