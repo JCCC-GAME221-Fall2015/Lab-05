b2a77144f50e731e0fbe7ed78cde56c9b9ac9e89
Darrick2theend <Darrick2theend@gmail.com>
2015-09-30 08:16:08 -0500

    Complete Lab 5

---
+++ Lab05-Networking/Assets/Scripts/PlayerNetworSetup.cs
@@ -0,0 +1,25 @@
+﻿/**
+ * @author Darrick Hilburn
+ * 
+ * This script initializes player components in the network.
+ */
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class PlayerNetworSetup : NetworkBehaviour 
+{
+    [SerializeField] AudioListener audioListener;
+
+	// Use this for initialization
+	void Start () 
+    {
+        if (isLocalPlayer)
+        {
+            GetComponent<CharacterController>().enabled = true;
+            GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = true;
+            GetComponentInChildren<Camera>().enabled = true;
+            audioListener.enabled = true;
+        }
+	}
+}
---
+++ Lab05-Networking/Assets/Scripts/PlayerSyncPosition.cs
@@ -0,0 +1,52 @@
+﻿/**
+ * @author Darrick Hilburn
+ * 
+ * This script synchronizes player position data on the network.
+ */
+
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class PlayerSyncPosition : NetworkBehaviour 
+{
+    // Reference to this player's transform
+    [SerializeField] Transform myTransform;
+    // Speed to interpolate position at
+    [SerializeField] float lerpRate = 15;
+    // Position according to the server
+    [SyncVar] private Vector3 syncPos;
+
+    void FixedUpdate()
+    {
+        TransmitPosition();
+        LerpPosition();
+    }
+
+    void LerpPosition()
+    {
+        if (!isLocalPlayer)
+            myTransform.position = Vector3.Lerp(myTransform.position, syncPos, Time.deltaTime * lerpRate);
+    }
+
+    /**
+     * CmdProvidePositionToServer tells the server where this player is in the network
+     * 
+     * @var pos: Player position in the server game scene
+     */
+    [Command]
+    void CmdProvidePositionToServer(Vector3 pos)
+    {
+        syncPos = pos;
+    }
+
+    /**
+     * TransmitPosition is a client call to the server
+     */
+    [ClientCallback]
+    void TransmitPosition()
+    {
+        if(isLocalPlayer)
+            CmdProvidePositionToServer(myTransform.position);
+    }
+}
