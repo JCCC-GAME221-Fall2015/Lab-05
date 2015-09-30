356726509b3bb5f268272e35f044b889b555b95c
agentronin316 <agentronin316@gmail.com>
2015-09-30 07:28:13 -0500

    At 18 minute mark
    Something is broken with the character controller, had a problem with it
    at import, will try to fix later. Camera is not enabling, need to double
    check how it was disabled.

---
+++ Lab 5/Assets/PlayerNetworkSetup.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerNetworkSetup : NetworkBehaviour {
+
+    [SerializeField]
+    Camera FPSCam;
+	// Use this for initialization
+	void Start ()
+    {
+        if (isLocalPlayer)
+        {
+            GameObject.Find("Main Camera").SetActive(false);
+            GetComponent<CharacterController>().enabled = true;
+            GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = true;
+            FPSCam.enabled = true;
+            FPSCam.GetComponent<AudioListener>().enabled = true;
+        }
+	}
+	
+}
