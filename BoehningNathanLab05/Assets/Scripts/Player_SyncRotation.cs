dbbe33e9560ac54276163076991bb39e099862bd
nboehning <nboehning@gmail.com>
2015-09-29 20:53:30 -0500

    Completed Networking lab
    Completed the networking lab

---
+++ BoehningNathanLab05/Assets/Scripts/Player_SyncRotation.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Player_SyncRotation : NetworkBehaviour
+{
+
+    [SyncVar] private Quaternion syncPlayerRotation;
+    [SyncVar] private Quaternion syncCamRotation;
+
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private Transform camTransform;
+    [SerializeField] private float lerpRate = 15;
+    
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+	    
+        TransmitRotations();
+        LerpRotations();
+
+	}
+
+    void LerpRotations()
+    {
+        if (!isLocalPlayer)
+        {
+            playerTransform.rotation = Quaternion.Lerp(playerTransform.rotation, syncPlayerRotation,
+                Time.deltaTime*lerpRate);
+            camTransform.rotation = Quaternion.Lerp(camTransform.rotation, syncCamRotation, Time.deltaTime*lerpRate);
+        }
+    }
+
+    [Command]
+    void CmdProvideRotationsToServer(Quaternion playerRotation, Quaternion camRotation)
+    {
+        syncPlayerRotation = playerRotation;
+        syncCamRotation = camRotation;
+    }
+
+    [Client]
+    void TransmitRotations()
+    {
+        if (isLocalPlayer)
+        {
+            CmdProvideRotationsToServer(playerTransform.rotation, camTransform.rotation);
+        }
+    }
+}
